6094911d18e76f26cadf5e2804a53cc117565f73
dh-free <hao.ding@insentek.com>
2024-02-04 09:51:08 +0800

    重构代码逻辑

--- fs-common/src/main/java/com/free/fs/common/storage/StorageTenantManager.java
+++ fs-common/src/main/java/com/free/fs/common/storage/StorageTenantManager.java
@@ -4,6 +4,7 @@ import com.free.fs.common.constant.CommonConstant;
 import com.free.fs.common.exception.StorageTenantException;
 import com.free.fs.common.utils.StringUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.ObjectUtils;
 
 /**
  * 存储租户管理
@@ -14,15 +15,15 @@ import jakarta.servlet.http.HttpServletRequest;
 public class StorageTenantManager {
 
     public static void setTenant(HttpServletRequest request) {
-        String tenantId = getTenant(request);
-        request.setAttribute(CommonConstant.STORAGE_TENANT_REQUEST, tenantId);
+        String tenant = request.getHeader(CommonConstant.STORAGE_TENANT_REQUEST);
+        request.setAttribute(CommonConstant.STORAGE_TENANT_REQUEST, tenant);
     }
 
     public static String getTenant(HttpServletRequest request) {
-        String tenant = request.getHeader(CommonConstant.STORAGE_TENANT_REQUEST);
-        if (StringUtil.isBlank(tenant)) {
+        Object tenant = request.getAttribute(CommonConstant.STORAGE_TENANT_REQUEST);
+        if (ObjectUtils.isEmpty(tenant)) {
             throw new StorageTenantException("Missing tenant identification");
         }
-        return tenant;
+        return (String) tenant;
     }
 }
